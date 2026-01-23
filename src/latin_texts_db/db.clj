3e49e26ffea6584fa862a7cd44c2d2a6709e6ecf
Aaron Decker <decker49@gmail.com>
2026-01-23 13:20:22 -0600

    Insert tokens as doubly-linked list.

--- src/latin_texts_db/db.clj
+++ src/latin_texts_db/db.clj
@@ -5,6 +5,8 @@
             [honey.sql.helpers :as h]
             [latin-texts.migrations.basic-tables]))
 
+;; useful HoneySQL ref: https://github.com/seancorfield/honeysql/blob/develop/doc/clause-reference.md
+
 (def db-spec
   {:dbtype "sqlite"
    :dbname "resources/db/latin.db"})   ;; relative to project root
@@ -27,16 +29,22 @@
       (throw e))))
 
 (defn insert-token-into-db* [text-id prev-id tokens]
-  (let [insert-result {:text_id text-id
-                       :wordform (first tokens)
-                       :prev_token_id prev-id
-                       :next_token_id nil ;; TODO
-                       }
-        new-id :todo]
-    (when (not (empty (rest tokens)))
-      (insert-token-into-db* new-id (rest tokens))
-      ))
-  )
+  (println "On " (first tokens))
+  (let [ ;; TODO handle puncutation
+        insert-result (do! {:insert-into [:tokens]
+                            :values [{:text_id text-id
+                                      :wordform (first tokens)
+                                      :prev_token_id prev-id
+                                      }]
+                            :returning :token_id})
+        new-token-id (:tokens/token_id (first insert-result))]
+    (do! {:update :tokens
+          :set {:next_token_id new-token-id}
+          :where [:= :token_id prev-id]})
+    (println "new-token-id" new-token-id)
+    (when (not (empty? (rest tokens)))
+      (insert-token-into-db* text-id new-token-id (rest tokens))
+      )))
 
 (defn insert-text! [text-title text-contents-as-string]
   (let [tokens (clojure.string/split text-contents-as-string #" ")
@@ -46,6 +54,6 @@
         text-id (:texts/text_id (first text-insert-result))]
     (when (nil? text-id)
       (throw (new Exception "text-id not found after attempted insert")))
-    (println "text-id: " text-id)
-    ;; (insert-token-into-db* nil tokens)
+    (println "text-id: " text-id tokens)
+    (insert-token-into-db* text-id nil tokens)
     ))
