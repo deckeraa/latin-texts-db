07bceaccd7261e044e15722adbcb3c5095f17121
Aaron Decker <decker49@gmail.com>
2026-01-23 14:40:42 -0600

    Create genitive->declension.

--- src/latin_texts_db/bulk_noun_insert.clj
+++ src/latin_texts_db/bulk_noun_insert.clj
@@ -7,6 +7,14 @@
    [latin-texts.migrations.basic-tables]
    [latin-texts.db :refer [ds do!]]))
 
+(defn genitive->declension [gen]
+  (cond
+    (clojure.string/ends-with? gen "ae") 1
+    (clojure.string/ends-with? gen "ī") 2
+    (clojure.string/ends-with? gen "is") 3
+    (clojure.string/ends-with? gen "ūs") 4
+    (clojure.string/ends-with? gen "ēs") 5))
+
 (defn get-noun-forms-first-declension [nom gen gender gloss]
   (let [stem (subs nom 0 (dec (count nom)))]
     [{:wordform (str stem "a") :gloss gloss :part_of_speech "noun" :number "singular" :gender gender :case_ "nominative"}
@@ -36,8 +44,9 @@
       (do! {:insert-into [:meanings]
             :values [meaning-values]}))))
 
-(defn insert-noun-meanings! [dict-entry declension gender gloss]
+(defn insert-noun-meanings! [dict-entry gender gloss]
   (let [[nom gen] (clojure.string/split dict-entry #", ")
+        declension (genitive->declension gen)
         meanings (case declension
                    1 (get-noun-forms-first-declension nom gen gender gloss))]
     (doseq [meaning meanings]
