3b1219082d6022aaf7e6ce3c7de4277497645e77
Aaron Decker <decker49@gmail.com>
2026-01-23 18:00:07 -0600

    Add ability to set current token.

--- src/cljs/latin_texts/app.cljs
+++ src/cljs/latin_texts/app.cljs
@@ -7,64 +7,84 @@
             [cljs.reader :as reader]
             ))
 
-;; (defn fetch-text [text-id]
-;;   (let [ch (chan)]
-;;     (-> (js/fetch (str "/text-as-string?text-id=" text-id))
-;;         (.then #(.json %))                    ;; or .text()
-;;         (.then #(put! ch (->clj %)))          ;; ← nice with js→cljs
-;;         (.catch #(put! ch {:error %})))
-;;     ch)
-;;   )
-
-;; (defn fetch-text [text-id]
-;;   (p/-> (js/fetch (str "/text-as-string?text-id=" text-id))
-;;         .json
-;;         ->clj))
-
+(defonce app-state (r/atom {}))
 
 (defn fetch-text [text-id]
   (-> (js/fetch (str "/text?text-id=" text-id))
       (.then (fn [v]
                (println v)
-               ;; (.text v)
-               ;; (reader/read-string (.text v))
                (.text v)
+               ))))
 
-               ;;(.json v)
-               ))                    ;; returns another promise
-      ;(.then ->clj)
-      ))     
+(defn current-token-id []
+  (:tokens/token_id (:current-token @app-state)))
+
+(defn set-current-token! [token]
+  (swap! app-state assoc :current-token token))
+
+(defn token-color [token]
+  "purple")
+
+(defn token-bg-color [token]
+  (when-let [id (:tokens/token_id token)]
+    (when (= id (current-token-id))
+      "yellow")))
 
 (defn text-fetcher-component []
-  (r/with-let [text-edn (r/atom [])]
-    [:div
-     [:div {} (str @text-edn) (count @text-edn)]
-     [:button {:on-click
-               (fn []
-                 (-> (fetch-text 1)
-                     (p/then (fn [result]
-                               (println "Got result: " result)
-                               (reset! text-edn (reader/read-string result))))
-                     (p/catch (fn [err]
-                                (println err)))))}
-      "Fetch"]
-     [:button {:on-click #(println (reader/read-string @text-edn))} "Parse"]
-     (into [:<>]
-           (map (fn [token]
-                  [:div (str
-                         (:tokens/punctuation_preceding token)
-                         (:tokens/wordform token)
-                         (:tokens/punctuation_trailing token))])
-                @text-edn))
-     ]))
+  [:div
+   [:button {:on-click
+             (fn []
+               (-> (fetch-text 1)
+                   (p/then (fn [result]
+                             (swap! app-state assoc :text (reader/read-string result))))
+                   (p/catch (fn [err]
+                              (println err)))))}
+    "Fetch"]
+   (into [:<>]
+         (map (fn [token]
+                ^{:key (:tokens/token_id token)}
+                [:span
+                 {:style {:color (token-color token)
+                          :background-color (token-bg-color token)
+                          :margin-right "6px"}
+                  :on-click #(set-current-token! token)}
+                 (str
+                  (:tokens/punctuation_preceding token)
+                  (:tokens/wordform token)
+                  (:tokens/punctuation_trailing token))])
+              (:text @app-state)))
+   ])
 
 (defn root-component []
   [:div
    [:h1 "Latin Texts DB"]
    [text-fetcher-component]
+   [:div {} @app-state]
    ])
 
+(defonce react-root (atom nil))
+
+(defn render! []
+  (let [root-el (js/document.getElementById "app")]
+    (when-not @react-root
+      (reset! react-root (rd-client/create-root root-el)))
+    (rd-client/render @react-root [root-component])))
+
 (defn ^:export init []
-  (let [root-el (js/document.getElementById "app")
-        react-root (rd-client/create-root root-el)]
-    (rd-client/render react-root [root-component])))
+  (render!))
+
+(defn ^:dev/before-load-async stop [done]
+  (js/console.log "stop")
+  (js/setTimeout
+    (fn []
+      (js/console.log "stop complete")
+      (render!)
+      (done))))
+
+(defn ^:dev/after-load-async start [done]
+  (js/console.log "start")
+  (js/setTimeout
+    (fn []
+      (js/console.log "start complete")
+      (render!)
+      (done))))
