141d7529385019b4cb67fdd446fc4281cb642214
Aaron Decker <decker49@gmail.com>
2026-01-23 21:50:52 -0600

    Add hic, haec, hoc

--- src/latin_texts_db/bulk_insert_irregular.clj
+++ src/latin_texts_db/bulk_insert_irregular.clj
@@ -5,11 +5,53 @@
    [honey.sql :as sql]
    [honey.sql.helpers :as h]
    [latin-texts-db.migrations.basic-tables]
-   [latin-texts-db.db :refer [ds do!]]))
+   [latin-texts-db.db :refer [ds do! ll]]))
 
 (def meanings-to-insert
-  [{:wordform "est" :gloss "he/she/it is" :part_of_speech "verb" :person 3 :number "singular" :tense "present" :mood "indicative" :voice "active"}
-   {:wordform "sunt" :gloss "they are" :part_of_speech "verb" :person 3 :number "plural" :tense "present" :mood "indicative" :voice "active"}])
+  [;; sum, es, ...
+   {:wordform "sum" :gloss "I am" :part_of_speech "verb" :person 1 :number "singular" :tense "present" :mood "indicative" :voice "active" :lexeme_id (ll "sum, esse, fuī, futūrum")}
+   {:wordform "es" :gloss "you are" :part_of_speech "verb" :person 2 :number "singular" :tense "present" :mood "indicative" :voice "active" :lexeme_id (ll "sum, esse, fuī, futūrum")}
+   {:wordform "est" :gloss "he/she/it is" :part_of_speech "verb" :person 3 :number "singular" :tense "present" :mood "indicative" :voice "active" :lexeme_id (ll "sum, esse, fuī, futūrum")}
+   {:wordform "sumus" :gloss "we are" :part_of_speech "verb" :person 1 :number "plural" :tense "present" :mood "indicative" :voice "active" :lexeme_id (ll "sum, esse, fuī, futūrum")}
+   {:wordform "estis" :gloss "you are" :part_of_speech "verb" :person 2 :number "plural" :tense "present" :mood "indicative" :voice "active" :lexeme_id (ll "sum, esse, fuī, futūrum")}
+   {:wordform "sunt" :gloss "they are" :part_of_speech "verb" :person 3 :number "plural" :tense "present" :mood "indicative" :voice "active" :lexeme_id (ll "sum, esse, fuī, futūrum")}
+   ;; hic, haec, hoc
+   {:wordform "hic" :gloss "this" :part_of_speech "noun" :number "singular" :gender "masculine" :case_ "nominative" :lexeme_id (ll "hic, haec, hoc")}
+   {:wordform "huius" :gloss "of this" :part_of_speech "noun" :number "singular" :gender "masculine" :case_ "genitive" :lexeme_id (ll "hic, haec, hoc")}
+   {:wordform "huic" :gloss "this" :part_of_speech "noun" :number "singular" :gender "masculine" :case_ "dative" :lexeme_id (ll "hic, haec, hoc")}
+   {:wordform "hunc" :gloss "this" :part_of_speech "noun" :number "singular" :gender "masculine" :case_ "accusative" :lexeme_id (ll "hic, haec, hoc")}
+   {:wordform "hōc" :gloss "this" :part_of_speech "noun" :number "singular" :gender "masculine" :case_ "ablative" :lexeme_id (ll "hic, haec, hoc")}
+
+   {:wordform "haec" :gloss "this" :part_of_speech "noun" :number "singular" :gender "feminine" :case_ "nominative" :lexeme_id (ll "hic, haec, hoc")}
+   {:wordform "huius" :gloss "of this" :part_of_speech "noun" :number "singular" :gender "feminine" :case_ "genitive" :lexeme_id (ll "hic, haec, hoc")}
+   {:wordform "huic" :gloss "this" :part_of_speech "noun" :number "singular" :gender "feminine" :case_ "dative" :lexeme_id (ll "hic, haec, hoc")}
+   {:wordform "hanc" :gloss "this" :part_of_speech "noun" :number "singular" :gender "feminine" :case_ "accusative" :lexeme_id (ll "hic, haec, hoc")}
+   {:wordform "hāc" :gloss "this" :part_of_speech "noun" :number "singular" :gender "feminine" :case_ "ablative" :lexeme_id (ll "hic, haec, hoc")}
+
+   {:wordform "hoc" :gloss "this" :part_of_speech "noun" :number "singular" :gender "neuter" :case_ "nominative" :lexeme_id (ll "hic, haec, hoc")}
+   {:wordform "huius" :gloss "of this" :part_of_speech "noun" :number "singular" :gender "neuter" :case_ "genitive" :lexeme_id (ll "hic, haec, hoc")}
+   {:wordform "huic" :gloss "this" :part_of_speech "noun" :number "singular" :gender "neuter" :case_ "dative" :lexeme_id (ll "hic, haec, hoc")}
+   {:wordform "hoc" :gloss "this" :part_of_speech "noun" :number "singular" :gender "neuter" :case_ "accusative" :lexeme_id (ll "hic, haec, hoc")}
+   {:wordform "hōc" :gloss "this" :part_of_speech "noun" :number "singular" :gender "neuter" :case_ "ablative" :lexeme_id (ll "hic, haec, hoc")}
+
+   {:wordform "hī" :gloss "these" :part_of_speech "noun" :number "singular" :gender "masculine" :case_ "nominative" :lexeme_id (ll "hic, haec, hoc")}
+   {:wordform "hōrum" :gloss "of these" :part_of_speech "noun" :number "singular" :gender "masculine" :case_ "genitive" :lexeme_id (ll "hic, haec, hoc")}
+   {:wordform "hīs" :gloss "these" :part_of_speech "noun" :number "singular" :gender "masculine" :case_ "dative" :lexeme_id (ll "hic, haec, hoc")}
+   {:wordform "hōs" :gloss "these" :part_of_speech "noun" :number "singular" :gender "masculine" :case_ "accusative" :lexeme_id (ll "hic, haec, hoc")}
+   {:wordform "hīs" :gloss "these" :part_of_speech "noun" :number "singular" :gender "masculine" :case_ "ablative" :lexeme_id (ll "hic, haec, hoc")}
+
+   {:wordform "hae" :gloss "these" :part_of_speech "noun" :number "singular" :gender "feminine" :case_ "nominative" :lexeme_id (ll "hic, haec, hoc")}
+   {:wordform "hārum" :gloss "of these" :part_of_speech "noun" :number "singular" :gender "feminine" :case_ "genitive" :lexeme_id (ll "hic, haec, hoc")}
+   {:wordform "hīs" :gloss "these" :part_of_speech "noun" :number "singular" :gender "feminine" :case_ "dative" :lexeme_id (ll "hic, haec, hoc")}
+   {:wordform "hās" :gloss "these" :part_of_speech "noun" :number "singular" :gender "feminine" :case_ "accusative" :lexeme_id (ll "hic, haec, hoc")}
+   {:wordform "hīs" :gloss "these" :part_of_speech "noun" :number "singular" :gender "feminine" :case_ "ablative" :lexeme_id (ll "hic, haec, hoc")}
+
+   {:wordform "haec" :gloss "these" :part_of_speech "noun" :number "singular" :gender "neuter" :case_ "nominative" :lexeme_id (ll "hic, haec, hoc")}
+   {:wordform "hōrum" :gloss "of these" :part_of_speech "noun" :number "singular" :gender "neuter" :case_ "genitive" :lexeme_id (ll "hic, haec, hoc")}
+   {:wordform "hīs" :gloss "these" :part_of_speech "noun" :number "singular" :gender "neuter" :case_ "dative" :lexeme_id (ll "hic, haec, hoc")}
+   {:wordform "haec" :gloss "these" :part_of_speech "noun" :number "singular" :gender "neuter" :case_ "accusative" :lexeme_id (ll "hic, haec, hoc")}
+   {:wordform "hīs" :gloss "these" :part_of_speech "noun" :number "singular" :gender "neuter" :case_ "ablative" :lexeme_id (ll "hic, haec, hoc")}
+   ])
 
 (defn insert-meaning! [meaning-values]
   (let [existing-match (do! {:select [:meaning_id]
