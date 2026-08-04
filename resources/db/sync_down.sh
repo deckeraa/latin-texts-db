95bdfe00674f5096e7dfdb7ec584ae97c781263d
Aaron Decker <decker49@gmail.com>
2026-08-04 09:46:35 -0500

    Add exit on sync_down if a backup copy can't be made.

--- resources/db/sync_down.sh
+++ resources/db/sync_down.sh
@@ -2,6 +2,7 @@
 # do a backup first so that progress can't be accidentally lost.
 TIMESTAMP=$(date +%Y%m%d-%H%M%S)
 BACKUP_NAME="${TIMESTAMP}_from_sync_latin.db"
-cp ./latin.db $BACKUP_NAME
-scp root@143.198.140.191:/home/sync/latin.db .
+cp ./latin.db $BACKUP_NAME || exit 1
 
+# now copy the file
+scp root@143.198.140.191:/home/sync/latin.db .
